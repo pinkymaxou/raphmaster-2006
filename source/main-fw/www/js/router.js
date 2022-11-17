df1adef1d85316caeb0ae2dce84261aa09e9e448
pinkymaxou <maxime.carrier.1987@gmail.com>
2022-11-16 21:00:29 -0500

    Feature/maxou/www calibration (#8)
    * Slot ingredient configuration
    
    * Slot settings
    
    Co-authored-by: Max <pinkymaxou08@gmail.com>

--- source/main-fw/www/js/router.js
+++ source/main-fw/www/js/router.js
@@ -3,6 +3,7 @@ import SettingsPage from "./views/SettingsPage.js";
 import AboutPage from "./views/AboutPage.js";
 import NetworkPage from "./views/NetworkPage.js";
 import CalibPage from "./views/CalibPage.js";
+import SlotSettingsPage from "./views/SlotSettingsPage.js";
 
 /* https://dcode.domenade.com/tutorials/build-a-single-page-app-with-javascript-no-frameworks */
 
@@ -28,6 +29,7 @@ const router = async () => {
         { path: "/network", view: NetworkPage },
         { path: "/settings", view: SettingsPage },
         { path: "/calib", view: CalibPage },
+        { path: "/slotsettings", view: SlotSettingsPage },
         { path: "/about", view: AboutPage },
     ];
 
---
+++ source/main-fw/www/js/views/SlotSettingsPage.js
@@ -0,0 +1,117 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor(params) {
+        super(params);
+        this.setTitle("Slot settings");
+    }
+
+    addIngredient(id, name) {
+        let cboNewIngredientOpt = document.createElement("option");
+        cboNewIngredientOpt.setAttribute("value", id);
+        cboNewIngredientOpt.text = name;
+        return cboNewIngredientOpt;
+    }
+
+    async loaded() {
+
+        let tbodySysInfo = document.querySelector("#tblBdSlots");
+
+        let ingredients = [
+            { id:  1, name: "Vokda" },           
+            { id:  2, name: "Peach Schnapps" },           
+        ];
+
+        let slotItems = [
+            { id:  1, totalQtyml: 0, remainingQtyml: 0 },
+            { id:  2, totalQtyml: 0, remainingQtyml: 0 },
+            { id:  3, totalQtyml: 0, remainingQtyml: 0 },
+            { id:  4, totalQtyml: 0, remainingQtyml: 0 },
+ 
+            { id:  5, totalQtyml: 0, remainingQtyml: 0 },
+            { id:  6, totalQtyml: 0, remainingQtyml: 0 },
+            { id:  7, totalQtyml: 0, remainingQtyml: 0 },
+            { id:  8, totalQtyml: 0, remainingQtyml: 0 },
+ 
+            { id:  9, totalQtyml: 0, remainingQtyml: 0 },
+            { id: 10, totalQtyml: 0, remainingQtyml: 0 },
+            { id: 11, totalQtyml: 0, remainingQtyml: 0 },
+            { id: 12, totalQtyml: 0, remainingQtyml: 0 },
+
+            { id: 13, totalQtyml: 0, remainingQtyml: 0 },
+            { id: 14, totalQtyml: 0, remainingQtyml: 0 },
+            { id: 15, totalQtyml: 0, remainingQtyml: 0 },
+            { id: 16, totalQtyml: 0, remainingQtyml: 0 },
+        ];
+
+        // Add info items
+        let i = 0;
+
+        slotItems.forEach(
+            (slotItem) =>
+            {
+                let newTr = tbodySysInfo.insertRow();
+
+                // Alternate row
+                if (i % 2 == 0) {
+                    newTr.classList.add("pure-table-odd");
+                }
+
+                // =====================
+                const tdSlot = newTr.insertCell(); // create td only
+                tdSlot.appendChild(document.createTextNode(slotItem.id));
+                
+                // =====================
+                const tdValue = newTr.insertCell(); // create td only
+                // Add select
+                let cboSelectIngredient = document.createElement("select");
+                
+                // Ingredients
+                cboSelectIngredient.appendChild(this.addIngredient(0, "--- None ---"));
+                ingredients.forEach( (ingredient) => cboSelectIngredient.appendChild(this.addIngredient(ingredient.id, ingredient.name)) );                
+                tdValue.appendChild(cboSelectIngredient);
+ 
+                // =====================
+                // Create textbox for total (qty)
+                const tdValueTotalQtyML = newTr.insertCell(); // create td only
+                let btnValueTotalQtyML = document.createElement("input");
+                btnValueTotalQtyML.setAttribute("type", "number");
+                btnValueTotalQtyML.setAttribute("min", 250);
+                btnValueTotalQtyML.setAttribute("max", 4500);
+                btnValueTotalQtyML.setAttribute("value", slotItem.totalQtyml);
+                tdValueTotalQtyML.appendChild(btnValueTotalQtyML);
+
+                // =====================
+                // Create textbox for remaining (qty)
+                const tdValueRemainingQtyML = newTr.insertCell(); // create td only
+                let btnValueRemainingQtyML = document.createElement("input");
+                btnValueRemainingQtyML.setAttribute("type", "number");
+                btnValueRemainingQtyML.setAttribute("min", 250);
+                btnValueRemainingQtyML.setAttribute("max", 4500);
+                btnValueRemainingQtyML.setAttribute("value", slotItem.remainingQtyml);
+                tdValueRemainingQtyML.appendChild(btnValueRemainingQtyML);
+
+                i++;
+            });
+    }
+
+    async getHtml() {
+        return `
+        <table class="pure-table">
+            <thead>
+                <tr>
+                    <th>Slot #</th>
+                    <th>Ingredient</th>
+                    <th>Total Qty (ml)</th>
+                    <th>Used Qty (ml)</th>
+                </tr>
+            </thead>
+            <tbody id="tblBdSlots">
+                <!-- Space for table items -->
+            </tbody>
+        </table>
+        <br>
+        <button class="pure-button pure-button-primary">Save</button>
+        `;
+    }
+}
