68e72536d364fae90c28bf3ad608904a0b8a0652
Max <pinkymaxou08@gmail.com>
2022-11-17 21:57:53 -0500

    Basic cocktail list

--- source/main-fw/www/js/views/IndexPage.js
+++ source/main-fw/www/js/views/IndexPage.js
@@ -12,36 +12,57 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-        <table class="pure-table">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Make</th>
-                <th>Model</th>
-                <th>Year</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Honda</td>
-                <td>Accord</td>
-                <td>2009</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Toyota</td>
-                <td>Camry</td>
-                <td>2012</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Hyundai</td>
-                <td>Elantra</td>
-                <td>2010</td>
-            </tr>
-            </tbody>
-        </table>
+        <div class="pure-g cocktail_item">
+            <img src="img/sex-on-the-beach.jpg" />
+            <div>
+                <p>Sex on the beach</p>
+                <ul>
+                    <li>ice</li>
+                    <li>50ml vodka</li>
+                    <li>25ml peach schnapps</li>
+                    <li>2 oranges, juiced, plus 2 slices to garnish</li>
+                    <li>50ml cranberry juice</li>
+                    <li>glacé cherries, to garnish (optional)</li>
+                </ul>
+            </div>
+            <div>
+                <button class="pure-button pure-button-primary">Make this</button>
+            </div>
+        </div>    
+        <div class="pure-g cocktail_item">
+            <img src="img/bloody-mary.jpg" />
+            <div>
+                <p>Bloody Mary</p>
+                <ul>
+                    <li>Celery salt</li>
+                    <li>1 lemon wedge</li>
+                    <li>1 lime wedge</li>
+                    <li>2 ounces vodka</li>
+                    <li>4 ounces tomato juice</li>
+                    <li>2 teaspoons prepared horseradish</li>
+                    <li>2 dashes Tabasco sauce</li>
+                    <li>2 dashes Worcestershire sauce</li>
+                    <li>1 pinch ground black pepper</li>
+                    <li>1 pinch smoked paprika</li>
+                    <li>Garnish: parsley sprig</li>
+                    <li>Garnish: green olives</li>
+                    <li>Garnish: lime wedge</li>
+                    <li>Garnish: celery stalk</li>
+                </ul>
+            </div>
+        </div>
+        <div class="pure-g cocktail_item">
+            <img src="img/pornstar.jpg" />
+            <div>
+                <p>Pornstar</p>
+                <ul>
+                    <li>Ice</li>
+                    <li>Curaçao bleu</li>
+                    <li>Sour Puss</li>
+                    <li>7-up</li>
+                </ul>
+            </div>
+        </div>
         `;
     }
 }
