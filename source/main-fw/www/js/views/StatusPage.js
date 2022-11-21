ec63bb801b637a0d2a756b2259b51c27ea867149
Max <pinkymaxou08@gmail.com>
2022-11-20 21:50:26 -0500

    Instruction to finish

--- source/main-fw/www/js/views/StatusPage.js
+++ source/main-fw/www/js/views/StatusPage.js
@@ -14,19 +14,36 @@ export default class extends AbstractView {
     async getHtml() {
         return `
         <div class="status_container">
-            <div class="status_userinstruction">
-                Please put the glass on the sleigh
+            <div style="display: flex">
+                <div class="lds-dual-ring" style="margin-top: 10px;"></div>
+                <div class="status_userinstruction">
+                    Please put your glass on the sleigh
+                </div>
             </div>
             <div class="progress-bar" style="max-width: 35rem;">
                 <span class="progress-bar-fill" style="width: 70%;"></span>
             </div>
             <div>
+                <p>Job: <b>Make Sex on the beach</b></p>
                 <ol>
-                    <li>Moving to the "White Rhum" slot</li>
-                    <li>Pouring "White Rhum"</li>
-                    <li>Moving to the "Vokda" slot</li>
+                    <li>Pour "White Rhum"</li>
+                    <li>Pour "Vokda"</li>
+                    <li>Pour "Grenadine"</li>
                     <li>Moving back to home</li>
                     <li>Waiting for user to take the glass</li>
+                </ol>
+            </div>
+
+            <!-- After the glass get removed. -->
+            <div>
+                <div class="status_userinstruction">
+                    Remaining instructions to finish "Sex on the beach"
+                </div>
+                <ol>
+                    <li>Ice</li>
+                    <li>Oranges wedge (2)</li>
+                    <li>Cranberry juice (50ml)</li>
+                    <li>Pour 7-up</li>
                 </ol>
             </div>
         </div>
