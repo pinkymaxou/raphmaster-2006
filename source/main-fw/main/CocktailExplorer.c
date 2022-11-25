b9395b80da78055fae83212a9348efb9a675f02f
Max <pinkymaxou08@gmail.com>
2022-11-24 20:25:08 -0500

    Cocktail explorer v1

--- source/main-fw/main/CocktailExplorer.c
+++ source/main-fw/main/CocktailExplorer.c
@@ -1,18 +1,17 @@
 #include "esp_log.h"
-#include "nanopb/pb_decode.h"
 
 #include "CocktailExplorer.h" 
 #include "assets/EmbedCocktailDb.h"
-#include "cocktaildb/cocktaildb.pb.h"
 
 #define TAG "CocktailExplorer"
 
+// Load everything into memory, it's easier
 static cocktaildb_IngredientFile m_sIngredientFile;
 static cocktaildb_RecipeFile m_sRecipeFile;
 
 void COCKTAILEXPLORER_Init()
 {
-    ESP_LOGI(TAG, "Loading ingredient");
+    ESP_LOGI(TAG, "Loading ingredient, block size: %d", sizeof(m_sIngredientFile));
 
     const EFEMBEDCOCKTAILDB_SFile* psIngredientFile = &EFEMBEDCOCKTAILDB_g_sFiles[EFEMBEDCOCKTAILDB_EFILE_INGREDIENTS_BIN];
 
@@ -24,9 +23,9 @@ void COCKTAILEXPLORER_Init()
         return;
     }
     
-    ESP_LOGI(TAG, "Loaded ingredient");
+    ESP_LOGI(TAG, "Loaded ingredient, total: %d", m_sIngredientFile.entries_count);
 
-    ESP_LOGI(TAG, "Loading recipes");
+    ESP_LOGI(TAG, "Loading recipes, block size: %d", sizeof(m_sRecipeFile));
 
     const EFEMBEDCOCKTAILDB_SFile* psRecipeFile = &EFEMBEDCOCKTAILDB_g_sFiles[EFEMBEDCOCKTAILDB_EFILE_RECIPES_BIN];
 
@@ -38,5 +37,15 @@ void COCKTAILEXPLORER_Init()
         return;
     }
     
-    ESP_LOGI(TAG, "Loaded recipes");
+    ESP_LOGI(TAG, "Loaded recipes, total: %d", m_sRecipeFile.entries_count);
+}
+
+const cocktaildb_IngredientFile* COCKTAILEXPLORER_GetIngredientFiles()
+{
+    return &m_sIngredientFile;
+}
+
+const cocktaildb_RecipeFile* COCKTAILEXPLORER_GetRecipeFiles()
+{
+    return &m_sRecipeFile;
 }
--- source/main-fw/main/CocktailExplorer.h
+++ source/main-fw/main/CocktailExplorer.h
@@ -1,6 +1,13 @@
 #ifndef _COCKTAILEXPLORER_H_
 #define _COCKTAILEXPLORER_H_
 
+#include "nanopb/pb_decode.h"
+#include "cocktaildb/cocktaildb.pb.h"
+
 void COCKTAILEXPLORER_Init();
+
+const cocktaildb_IngredientFile* COCKTAILEXPLORER_GetIngredientFiles();
+
+const cocktaildb_RecipeFile* COCKTAILEXPLORER_GetRecipeFiles();
 
 #endif
