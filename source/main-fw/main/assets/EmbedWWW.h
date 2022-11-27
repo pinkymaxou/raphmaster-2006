2232b823ebc8532d7cbf94c14389f02eb08e3dba
Max <pinkymaxou08@gmail.com>
2022-11-27 10:42:21 -0500

    update bin

--- source/main-fw/main/assets/EmbedWWW.h
+++ source/main-fw/main/assets/EmbedWWW.h
@@ -36,22 +36,30 @@ typedef enum
   EFEMBEDWWW_EFILE_FAVICON_ICO = 14,    /*!< @brief File: favicon.ico (size: 15.0 KB) */
   EFEMBEDWWW_EFILE_IMG_AMARETTO_SOUR_JPG = 15,    /*!< @brief File: img/amaretto-sour.jpg (size: 3.5 KB) */
   EFEMBEDWWW_EFILE_IMG_APEROL_SPRITZ_JPG = 16,    /*!< @brief File: img/aperol_spritz.jpg (size: 5.1 KB) */
-  EFEMBEDWWW_EFILE_IMG_BLOODY_MARY_JPG = 17,    /*!< @brief File: img/bloody-mary.jpg (size: 4.9 KB) */
-  EFEMBEDWWW_EFILE_IMG_DAIQUIRI_JPG = 18,    /*!< @brief File: img/daiquiri.jpg (size: 4.0 KB) */
-  EFEMBEDWWW_EFILE_IMG_GRASSHOPPER_JPG = 19,    /*!< @brief File: img/grasshopper.jpg (size: 3.1 KB) */
-  EFEMBEDWWW_EFILE_IMG_MARGARITA_JPG = 20,    /*!< @brief File: img/margarita.jpg (size: 2.9 KB) */
-  EFEMBEDWWW_EFILE_IMG_MARTINI_JPG = 21,    /*!< @brief File: img/martini.jpg (size: 3.1 KB) */
-  EFEMBEDWWW_EFILE_FONT_ORBITRON_BOLD_WOFF = 22,    /*!< @brief File: font/Orbitron-Bold.woff (size: 13.3 KB) */
-  EFEMBEDWWW_EFILE_FONT_ORBITRON_REGULAR_WOFF = 23,    /*!< @brief File: font/Orbitron-Regular.woff (size: 12.9 KB) */
-  EFEMBEDWWW_EFILE_IMG_PORNSTAR_JPG = 24,    /*!< @brief File: img/pornstar.jpg (size: 4.8 KB) */
-  EFEMBEDWWW_EFILE_FONT_ROBOTOMONO_BOLD_WOFF = 25,    /*!< @brief File: font/RobotoMono-Bold.woff (size: 52.4 KB) */
-  EFEMBEDWWW_EFILE_FONT_ROBOTOMONO_REGULAR_WOFF = 26,    /*!< @brief File: font/RobotoMono-Regular.woff (size: 52.1 KB) */
-  EFEMBEDWWW_EFILE_IMG_SEX_ON_THE_BEACH_JPG = 27,    /*!< @brief File: img/sex-on-the-beach.jpg (size: 5.7 KB) */
-  EFEMBEDWWW_EFILE_IMG_SKITTLE_JPG = 28,    /*!< @brief File: img/skittle.jpg (size: 973 B) */
-  EFEMBEDWWW_EFILE_IMG_TOM_COLLINS_JPG = 29,    /*!< @brief File: img/tom-collins.jpg (size: 3.0 KB) */
-  EFEMBEDWWW_EFILE_IMG_SVG_WIFI_SVG = 30,    /*!< @brief File: img/svg/wifi.svg (size: 795 B) */
-  EFEMBEDWWW_EFILE_IMG_SVG_WRENCH_SVG = 31,    /*!< @brief File: img/svg/wrench.svg (size: 713 B) */
-  EFEMBEDWWW_EFILE_COUNT = 32
+  EFEMBEDWWW_EFILE_IMG_SVG_BARS_SVG = 17,    /*!< @brief File: img/svg/bars.svg (size: 601 B) */
+  EFEMBEDWWW_EFILE_IMG_BLOODY_MARY_JPG = 18,    /*!< @brief File: img/bloody-mary.jpg (size: 4.9 KB) */
+  EFEMBEDWWW_EFILE_IMG_DAIQUIRI_JPG = 19,    /*!< @brief File: img/daiquiri.jpg (size: 4.0 KB) */
+  EFEMBEDWWW_EFILE_IMG_SVG_FILE_EXPORT_SVG = 20,    /*!< @brief File: img/svg/file-export.svg (size: 626 B) */
+  EFEMBEDWWW_EFILE_IMG_SVG_FILE_IMPORT_SVG = 21,    /*!< @brief File: img/svg/file-import.svg (size: 630 B) */
+  EFEMBEDWWW_EFILE_IMG_SVG_GLASS_MARTINI_ALT_SVG = 22,    /*!< @brief File: img/svg/glass-martini-alt.svg (size: 503 B) */
+  EFEMBEDWWW_EFILE_IMG_SVG_GLASS_WHISKEY_SVG = 23,    /*!< @brief File: img/svg/glass-whiskey.svg (size: 439 B) */
+  EFEMBEDWWW_EFILE_IMG_GRASSHOPPER_JPG = 24,    /*!< @brief File: img/grasshopper.jpg (size: 3.1 KB) */
+  EFEMBEDWWW_EFILE_IMG_SVG_INFO_SVG = 25,    /*!< @brief File: img/svg/info.svg (size: 568 B) */
+  EFEMBEDWWW_EFILE_IMG_SVG_LANGUAGE_SVG = 26,    /*!< @brief File: img/svg/language.svg (size: 1.2 KB) */
+  EFEMBEDWWW_EFILE_IMG_MARGARITA_JPG = 27,    /*!< @brief File: img/margarita.jpg (size: 2.9 KB) */
+  EFEMBEDWWW_EFILE_IMG_MARTINI_JPG = 28,    /*!< @brief File: img/martini.jpg (size: 3.1 KB) */
+  EFEMBEDWWW_EFILE_FONT_ORBITRON_BOLD_WOFF = 29,    /*!< @brief File: font/Orbitron-Bold.woff (size: 13.3 KB) */
+  EFEMBEDWWW_EFILE_FONT_ORBITRON_REGULAR_WOFF = 30,    /*!< @brief File: font/Orbitron-Regular.woff (size: 12.9 KB) */
+  EFEMBEDWWW_EFILE_IMG_PORNSTAR_JPG = 31,    /*!< @brief File: img/pornstar.jpg (size: 4.8 KB) */
+  EFEMBEDWWW_EFILE_IMG_SVG_POWER_OFF_SVG = 32,    /*!< @brief File: img/svg/power-off.svg (size: 723 B) */
+  EFEMBEDWWW_EFILE_FONT_ROBOTOMONO_BOLD_WOFF = 33,    /*!< @brief File: font/RobotoMono-Bold.woff (size: 52.4 KB) */
+  EFEMBEDWWW_EFILE_FONT_ROBOTOMONO_REGULAR_WOFF = 34,    /*!< @brief File: font/RobotoMono-Regular.woff (size: 52.1 KB) */
+  EFEMBEDWWW_EFILE_IMG_SEX_ON_THE_BEACH_JPG = 35,    /*!< @brief File: img/sex-on-the-beach.jpg (size: 5.7 KB) */
+  EFEMBEDWWW_EFILE_IMG_SKITTLE_JPG = 36,    /*!< @brief File: img/skittle.jpg (size: 973 B) */
+  EFEMBEDWWW_EFILE_IMG_TOM_COLLINS_JPG = 37,    /*!< @brief File: img/tom-collins.jpg (size: 3.0 KB) */
+  EFEMBEDWWW_EFILE_IMG_SVG_WIFI_SVG = 38,    /*!< @brief File: img/svg/wifi.svg (size: 795 B) */
+  EFEMBEDWWW_EFILE_IMG_SVG_WRENCH_SVG = 39,    /*!< @brief File: img/svg/wrench.svg (size: 713 B) */
+  EFEMBEDWWW_EFILE_COUNT = 40
 } EFEMBEDWWW_EFILE;
 
 /*! @brief Check if compressed flag is active */
